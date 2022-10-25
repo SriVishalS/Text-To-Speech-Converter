acf26490d0ef71b9be91149457fd86dc8085a49e
sriVishalS <vishalS221202@gmail.com>
2022-10-25 19:13:07 +0530

    Text-to-Speech

--- script.js
+++ script.js
@@ -1,57 +1,57 @@
 const textarea = document.querySelector("textarea"),
-voiceList = document.querySelector("select"),
-speechBtn = document.querySelector("button");
+  voiceList = document.querySelector("select"),
+  speechBtn = document.querySelector("button");
 
 let synth = speechSynthesis,
-isSpeaking = true;
+  isSpeaking = true;
 
 voices();
 
-function voices(){
-    for(let voice of synth.getVoices()){
-        let selected = voice.name === "Google US English" ? "selected" : "";
-        let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
-        voiceList.insertAdjacentHTML("beforeend", option);
-    }
+function voices() {
+  for (let voice of synth.getVoices()) {
+    let selected = voice.name === "Google US English" ? "selected" : "";
+    let option = `<option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+    voiceList.insertAdjacentHTML("beforeend", option);
+  }
 }
 
 synth.addEventListener("voiceschanged", voices);
 
-function textToSpeech(text){
-    let utterance = new SpeechSynthesisUtterance(text);
-    for(let voice of synth.getVoices()){
-        if(voice.name === voiceList.value){
-            utterance.voice = voice;
-        }
+function textToSpeech(text) {
+  let utterance = new SpeechSynthesisUtterance(text);
+  for (let voice of synth.getVoices()) {
+    if (voice.name === voiceList.value) {
+      utterance.voice = voice;
     }
-    synth.speak(utterance);
+  }
+  synth.speak(utterance);
 }
 
-speechBtn.addEventListener("click", e =>{
-    e.preventDefault();
-    if(textarea.value !== ""){
-        if(!synth.speaking){
-            textToSpeech(textarea.value);
-        }
-        if(textarea.value.length > 80){
-            setInterval(()=>{
-                if(!synth.speaking && !isSpeaking){
-                    isSpeaking = true;
-                    speechBtn.innerText = "Convert To Speech";
-                }else{
-                }
-            }, 500);
-            if(isSpeaking){
-                synth.resume();
-                isSpeaking = false;
-                speechBtn.innerText = "Pause Speech";
-            }else{
-                synth.pause();
-                isSpeaking = true;
-                speechBtn.innerText = "Resume Speech";
-            }
-        }else{
-            speechBtn.innerText = "Convert To Speech";
-        }
+speechBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (textarea.value !== "") {
+    if (!synth.speaking) {
+      textToSpeech(textarea.value);
     }
+    if (textarea.value.length > 80) {
+      setInterval(() => {
+        if (!synth.speaking && !isSpeaking) {
+          isSpeaking = true;
+          speechBtn.innerText = "Convert To Speech";
+        } else {
+        }
+      }, 500);
+      if (isSpeaking) {
+        synth.resume();
+        isSpeaking = false;
+        speechBtn.innerText = "Pause Speech";
+      } else {
+        synth.pause();
+        isSpeaking = true;
+        speechBtn.innerText = "Resume Speech";
+      }
+    } else {
+      speechBtn.innerText = "Convert To Speech";
+    }
+  }
 });
